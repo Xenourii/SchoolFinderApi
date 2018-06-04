f37d64646ee1639db64e2a37c4eca2a2b5b74788
ajalabert <jalabert.ad@gmail.com>
2018-06-04 16:22:15 +0200

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,9 @@ User.create({
     password_confirmation: 'user1234!'
   },
   {
-      email: 'admin@admin.com'
-      password: 'Passw0rd!'
-      password_confirmation: 'Passw0rd!'
+    email: 'admin@admin.com'
+    password: 'Passw0rd!'
+    password_confirmation: 'Passw0rd!'
   })
 
 School.create({
@@ -40,7 +40,7 @@ School.create({
     email: "contact@ynov.com",
     latitude: 45.7456831,
     longitude: 4.837593400000006,
-    student_number: 200,
+    student_number: 150,
     status: "private"
   },
   {
@@ -57,15 +57,67 @@ School.create({
     status: "public"
   },
   {
+    name: "Bellecour École",
+    address: "18 Rue François Dauphin",
+    zip_code: "69002",
+    city: "Lyon",
+    opening_hours: "De 8h45 à 20h",
+    phone_number: "0478929283",
+    email: "bellecour@lyon.com",
+    latitude: 45.7553203,
+    longitude: 4.832654700000035,
+    student_number: 100,
+    status: "private"
+  },
+  {
     name: "École centrale de Lyon",
     address: "36 Avenue Guy de Collongue",
     zip_code: "69134",
-    city: "Écully",
+    city: "Lyon",
     opening_hours: "De 8h30 à 19h",
     phone_number: "0472186000",
     email: "ecolecentral@lyon.com",
-    latitude: 48.862725,
-    longitude: 2.287592000000018,
-    student_number: 500,
+    latitude: 45.76501959999999,
+    longitude: 4.838126999999986,
+    student_number: 700,
     status: "public"
-});
+  },
+  {
+    name: "ESTBB - École Supérieure de Biologie, Biochimie, Biotechnologies",
+    address: "10 place des archives",
+    zip_code: "69002",
+    city: "Lyon",
+    opening_hours: "De 7h à 15h",
+    phone_number: "0472325198",
+    email: "estbb@lyon.com",
+    latitude: 45.746622,
+    longitude: 4.826411000000007,
+    student_number: 450,
+    status: "public"
+  },
+  {
+    name: "ECAM Lyon",
+    address: "40 Montée Saint-Barthélémy",
+    zip_code: "69321",
+    city: "Lyon",
+    opening_hours: "De 6h à 12h30",
+    phone_number: "0472770600",
+    email: "ecam@lyon.com",
+    latitude: 45.7644165,
+    longitude: 4.824784200000067,
+    student_number: 90,
+    status: "private"
+  },
+  {
+    name: "Ecole Publique Albert Camus",
+    address: "Quai Jean Moulin",
+    zip_code: "69002",
+    city: "Lyon",
+    opening_hours: "De 10h à 18h",
+    phone_number: "0478363613",
+    email: "albertcamus@lyon.com",
+    latitude: 45.7553203,
+    longitude: 4.832654700000035,
+    student_number: 20,
+    status: "public"
+  });
